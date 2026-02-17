81cc5d0d1bfac6e621b18952faf6a574483e79fe
Prachi-6921 <prachi02rani@gmail.com>
2026-02-17 13:33:22 +0530

    [Prachi Rani] added uc2: print Oops art

---
+++ src/OopsBanner1.java
@@ -0,0 +1,12 @@
+public class OopsBanner1{
+    public static void main(String []args){
+        //UC2:
+        System.out.println("   ***   " + " " + "   ***   " + " " + "******   " + "   ***** ");
+        System.out.println(" **   ** " + " " + " **   ** " + " " + "**    ** " + " **      ");
+        System.out.println("**     **" + " " + "**     **" + " " + "**    ** " + "**       ");
+        System.out.println("**     **" + " " + "**     **" + " " + "******   " + "  *****  ");
+        System.out.println("**     **" + " " + "**     **" + " " + "**       " + "      ** ");
+        System.out.println(" **   ** " + " " + " **   ** " + " " + "**       " + " **   ** ");
+        System.out.println("   ***   " + " " + "   ***   " + " " + "**       " + "  *****  ");
+    }
+}
