239b941947454f1ff5de0ef21d2975557b2b4c00
Prachi-6921 <prachi02rani@gmail.com>
2026-02-21 17:37:54 +0530

    [Prachi Rani] added uc3: print oops banner using string.join

---
+++ src/OopsBanner2.java
@@ -0,0 +1,12 @@
+public class OopsBanner2 {
+    public static void main(String[] args) {
+        //UC3:
+        System.out.println(String.join(" ","   ***   " , " " , "   ***   " , " " , " *****  " , " " , "   ***** "));
+        System.out.println(String.join(" "," **   ** " , " " , " **   ** " , " " , " **   **" , " " , " **      "));
+        System.out.println(String.join(" ","**     **" , " " , "**     **" , " " , " **   **" , " " , "**       "));
+        System.out.println(String.join(" ","**     **" , " " , "**     **" , " " , " *****  " , " " , "  *****  "));
+        System.out.println(String.join(" ","**     **" , " " , "**     **" , " " , " **     " , " " , "      ** "));
+        System.out.println(String.join(" "," **   ** " , " " , " **   ** " , " " , " **     " , " " , "**    ** "));
+        System.out.println(String.join(" ","   ***   " , " " , "   ***   " , " " , " **     " , " " , " *****   "));
+    }
+}
