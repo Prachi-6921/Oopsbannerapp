460337ffd4f1af7a34b3474b6ca0ccd0e7680d16
Prachi-6921 <prachi02rani@gmail.com>
2026-03-10 12:54:36 +0530

    [Prachi Rani] added uc7: Store character Pattern in a class

---
+++ src/OopsBanner6.java
@@ -0,0 +1,93 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class OopsBanner6 {
+
+    public static void main(String[] args) {
+
+        String word = "OOPS";
+
+        String[] banner = CharacterPatternMap.buildBanner(word);
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+
+    static class CharacterPattern {
+
+        private char character;
+        private String[] pattern;
+
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
+    static class CharacterPatternMap {
+
+        private static final Map<Character, CharacterPattern> patternMap = new HashMap<>();
+
+        static {
+            patternMap.put('O', new CharacterPattern('O', new String[]{
+                    "  ***  ",
+                    " *   * ",
+                    " *   * ",
+                    " *   * ",
+                    " *   * ",
+                    "  ***  ",
+                    "       "
+            }));
+
+            patternMap.put('P', new CharacterPattern('P', new String[]{
+                    " ****  ",
+                    " *   * ",
+                    " ****  ",
+                    " *     ",
+                    " *     ",
+                    " *     ",
+                    "       "
+            }));
+
+            patternMap.put('S', new CharacterPattern('S', new String[]{
+                    "  **** ",
+                    " *     ",
+                    "  ***  ",
+                    "     * ",
+                    "     * ",
+                    " ****  ",
+                    "       "
+            }));
+        }
+
+        public static String[] buildBanner(String word) {
+
+            int height = 7;
+            String[] output = new String[height];
+
+            for (int i = 0; i < height; i++) {
+                StringBuilder lineBuilder = new StringBuilder();
+
+                for (char ch : word.toCharArray()) {
+                    CharacterPattern pattern = patternMap.get(ch);
+                    if (pattern != null) {
+                        lineBuilder.append(pattern.getPattern()[i]).append("  ");
+                    }
+                }
+
+                output[i] = lineBuilder.toString();
+            }
+
+            return output;
+        }
+    }
+}
